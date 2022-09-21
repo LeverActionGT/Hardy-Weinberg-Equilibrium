40f8a29c62c756c57818a0bb0fcd9da2a9243aa8
LeverActionGT <63264121+LeverActionGT@users.noreply.github.com>
2022-09-20 20:47:04 -0400

    Rewrite
    Swapped over to genepool simulation over individual simulation

--- main.py
+++ main.py
@@ -2,28 +2,32 @@ import simulation
 import csv
 
 SETTINGS = {
-    'pop_size': 500,
-    'num_generations': 200,
-    'initial_p': 0.5,
-    'initial_q': 0.5
+    'pop_size': 100000,
+    'num_generations': 25,
+    'initial_p': 0.1,
+    'initial_q': 0.9,
+    'print_stats': False
 }
 
 FITNESS = {
-    'AA_fitness': 0.5,
+    'AA_fitness': .5,
     'Aa_fitness': 1,
-    'aa_fitness': 0.5 
+    'aa_fitness': 0 
 }
 
 def main():
+    # make sure p+q=1
+    SETTINGS['initial_q'] = 1 - SETTINGS['initial_p']
+
     sim = simulation.Simulation(SETTINGS, FITNESS)
     output = sim.run_simulation() # {gen#:{}} where {} is the stats for that generation
 
     # write the output to a csv file
     file = open('output/output.csv', 'w', newline='')
     writer = csv.writer(file)
-    writer.writerow(['Generation', 'size', 'p', 'q', 'p^2', 'q^2', '2pq'])
+    writer.writerow(['gen', 'p', 'q', 'p^2', 'q^2', '2pq'])
     for gen in output:
-        writer.writerow([gen, output[gen]['size'], output[gen]['p'], output[gen]['q'], output[gen]['p2'], output[gen]['q2'], output[gen]['2pq']])
+        writer.writerow([gen, output[gen]['p'], output[gen]['q'], output[gen]['p2'], output[gen]['q2'], output[gen]['2pq']])
 
 if __name__ == '__main__':
-    main()
+    main() 
--- simulation.py
+++ simulation.py
@@ -1,47 +1,95 @@
 import random
 
-import population
-import individual
-
 # create a simulation
 class Simulation:
     def __init__(self, settings, fitness):
         self.settings = settings
         self.fitness = fitness
-        self.pop = population.Population(self.settings['pop_size'])
-        self.generation = 0
         self.data = {}
         self.create_initial_generation()
-        
-    # record the stats for the current generation
-    def record_stats(self):
-        # get the population stats
-        stats = self.pop.get_stats()
-        self.data[self.generation] = stats
-        
 
-    # create the initial generation
+    # get the stats for the current generation
+    def get_stats(self):
+        p = self.genepool.count('A')/len(self.genepool)
+        q = self.genepool.count('a')/len(self.genepool)
+        return {
+            'p': round(p, 4),
+            'q': round(q, 4),
+            'p2': round(p*p, 4),
+            'q2': round(q*q, 4),
+            '2pq': round(2*p*q, 4)
+        }
+        
+    # create the initial generation    
     def create_initial_generation(self):
-        for i in range(self.settings['pop_size']):
-            genes = random.choices(['A', 'a'], weights=[self.settings['initial_p'], self.settings['initial_q']], k=2)
-            ind = individual.Individual(genes)
-            self.pop.add(ind)
-        self.record_stats()
-        print('Initial Generation Created')
+        self.generation = 0
+        A_alleles = round(self.settings['initial_p']*self.settings['pop_size'])
+        a_alleles = round(self.settings['initial_q']*self.settings['pop_size'])
+        self.genepool = ['A']*A_alleles + ['a']*a_alleles
+
+        # Record data for initial generation
+        self.data[self.generation] = self.get_stats()
+        self.print_stats(self.data[self.generation])
+
+    # reproduce the current generation
+    def reproduce(self):
+        new_genepool = []
+        kids = []
+
+        # create the next generation by randomly selecting parents
+        while len(kids) < self.settings['pop_size']:
+            mama = random.choice(self.genepool)
+            papa = random.choice(self.genepool)
+            kid = mama + papa
+            kids.append(kid)
+
+        # apply fitness to the next generation & add to the new genepool
+        for kid in kids:
+            if kid == 'AA':
+                if random.random() < self.fitness['AA_fitness']:
+                    new_genepool.append('A')
+                    new_genepool.append('A')
+            elif kid == 'Aa' or kid == 'aA':
+                if random.random() < self.fitness['Aa_fitness']:
+                    new_genepool.append('A')
+                    new_genepool.append('a')
+            elif kid == 'aa':
+                if random.random() < self.fitness['aa_fitness']:
+                    new_genepool.append('a')
+                    new_genepool.append('a')
+        return new_genepool
+
+    # print stats
+    def print_stats(self, stats):
+        if self.settings['print_stats']:
+            print(
+                'Generation' + str(self.generation) + ':',
+                '\tp: ' + str(stats['p']),
+                '\tq: ' + str(stats['q']),
+                '\tp^2: ' + str(stats['p2']),
+                '\tq^2: ' + str(stats['q2']),
+                '\t2pq: ' + str(stats['2pq'])
+            )
 
     # run the simulation
     def run_simulation(self):
         for i in range(self.settings['num_generations']):
             self.generation += 1
-            next_generation = self.pop.next_generation(self.fitness)
-            if self.pop.size < 2:
-                print(f'Population too small to reproduce at generation {self.generation}')
+            self.genepool = self.reproduce()
+            self.data[self.generation] = self.get_stats()
+            self.print_stats(self.data[self.generation])
+
+            # stop if an allele is extinct
+            if self.data[self.generation]['p'] == 0:
+                print(f' \nAllele A has gone extinct on generation {self.generation}!')
                 break
-            self.pop = next_generation
-            print(f'Generation {self.generation} created')
-            self.record_stats()
+            if self.data[self.generation]['q'] == 0:
+                print(f' \nAllele a has gone extinct on generation {self.generation}!')
+                break
 
-        # Simulation Complete
-        print('Simulation Complete')
+        # done
+        print(' \nSimulation complete!')
+
+        # report initial conditions
+        print(f' \nInitial Conditions:\n\tp: {self.settings["initial_p"]}\n\tq: {self.settings["initial_q"]}\n\nFitness:\n\tAA_fitness: {self.fitness["AA_fitness"]}\n\tAa_fitness: {self.fitness["Aa_fitness"]}\n\taa_fitness: {self.fitness["aa_fitness"]}')
         return self.data
-
